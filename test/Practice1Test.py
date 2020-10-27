f8140e3533290ff98c637add87c6d0515196713f
Kevin Hao <haokaibo@outlook.com>
2020-10-27 10:00:50 +0800

    add more tests for map function.

--- test/Practice1Test.py
+++ test/Practice1Test.py
@@ -58,3 +58,63 @@ class Practice1Test(TestCase):
 
         print(d.get('hello', 'default_value'))
         print(d.get('not exist key', 'default_value'))
+
+    def test_filter_array(self):
+        a = [3, 4, 5]
+        b = [i for i in a if i > 4]
+        print(b)
+
+        b = list(filter(lambda x: x > 4, a))
+        print(b)
+
+    def test_map_array(self):
+        a = [3, 4, 5]
+        b = list(map(lambda i: i + 3, a))
+        print(b)
+
+    def test_read_file(self):
+        with open('file.txt') as fp:
+            for line in fp.readlines():
+                print(line)
+
+    def test_multiple_lines_format(self):
+        long_string = 'For a long time I used to go to bed early. ' \
+                      'Sometimes, when I had put out my candle, ' \
+                      'my eyes would close so quickly that I had not even time to say “I’m going to sleep.”'
+        print(long_string)
+
+        long_string = (
+            "For a long time I used to go to bed early. Sometimes, "
+            "when I had put out my candle, my eyes would close so quickly "
+            "that I had not even time to say “I’m going to sleep.”"
+        )
+        print(long_string)
+
+    def test_make_complex(self):
+        x, y = 1, 2
+        print({'x': x, 'y': y})
+
+    def test_placeholder(self):
+        file_name = 'foobar.txt'
+        basename, _, ext = file_name.rpartition('.')
+        assert (f'basename={basename}, _={_}, ext={ext}' == 'basename=foobar, _=., ext=txt')
+
+    def test_map_addition(self):
+        def addition(n):
+            return n + n
+
+        numbers = (1, 2, 3, 4)
+        result = map(addition, numbers)
+        assert (list(result) == [2, 4, 6, 8])
+
+    def test_map_lambda(self):
+        numbers = (1, 2, 3, 4)
+        result = map(lambda x: x + x, numbers)
+        assert (list(result) == [2, 4, 6, 8])
+
+    def test_map_add_two_array(self):
+        numbers1 = [1, 2, 3]
+        numbers2 = [4, 5, 6]
+
+        result = map(lambda x, y: x + y, numbers1, numbers2)
+        assert (list(result) == [5, 7, 9])
